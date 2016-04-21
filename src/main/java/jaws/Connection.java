b231a0006eba995302dac069f8da86067b036820
Hallgeir Løkken <hallgeir@loekken.org>
2016-04-21 13:43:27 +0200

    Begun work on connection-class

---
+++ src/main/java/jaws/Connection.java
@@ -0,0 +1,32 @@
+package jaws;
+
+import java.io.*;
+import java.net.*;
+
+public class Connection extends Thread {
+
+    final Socket socket;
+
+    public Connenction(Socket socket) {
+        this.socket = socket;
+    }
+
+    @Override
+    public void run() {
+        try(
+                DataInputStream input = new DataInputStream(socket.getInputStream);
+                DataOutputStream output = new DataOutputStream(socket.getOutputStream);
+           )
+        {
+            byte[] header = new byte[2];
+            input.read(header, 0, header.length);
+
+            boolean fin = (header[0]>>7) != 0;
+
+        }
+        catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
+
