8c0bdec9ecabd2ced5c85d0c87904fab8f6fc05d
elodin <jorgen.skogstad@gmail.com>
2016-04-21 11:16:28 +0200

    added run script for easy test runs /lazy

--- src/main/java/jaws/JaWS.java
+++ src/main/java/jaws/JaWS.java
@@ -1,6 +1,6 @@
 package jaws;
 
-public class HelloSeaWorld {
+public class JaWS {
   public static void main(String[] args) {
     Greeter greeter = new Greeter();
     System.out.println(greeter.sayHello());
