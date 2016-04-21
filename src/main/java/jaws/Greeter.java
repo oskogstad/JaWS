f031e2971035cecd80598c9c2439eaded5e7117d
elodin <jorgen.skogstad@gmail.com>
2016-04-21 10:48:26 +0200

    set up gradle test

---
+++ src/main/java/jaws/Greeter.java
@@ -0,0 +1,7 @@
+package jaws;
+
+public class Greeter {
+  public String sayHello() {
+    return "Hello Sea World!";
+  }
+}
---
+++ src/main/java/jaws/HelloSeaWorld.java
@@ -0,0 +1,8 @@
+package jaws;
+
+public class HelloSeaWorld {
+  public static void main(String[] args) {
+    Greeter greeter = new Greeter();
+    System.out.println(greeter.sayHello());
+  }
+}
