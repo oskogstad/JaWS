292ca60ee473a98c9e92b0d65c31dc53f0ce58f6
Fredric Segtnan Mentzoni <menfre@mac.com>
2016-04-26 11:48:13 +0200

    Added comments on Logger.java

--- src/main/java/jaws/Logger.java
+++ src/main/java/jaws/Logger.java
@@ -2,12 +2,12 @@ package jaws;
 
 public class Logger {
 
-    public static final int NONE = 0x00;
-    public static final int WS_IO = 0x01;
-    public static final int JSON = 0x02;
-    public static final int GENERAL = 0x04;
-    public static final int WS_PARSE = 0x08;
-    public static final int ALL = 0xFF;
+    public static final int NONE = 0x00; // Disable printing. Use as loglevel, not as category.
+    public static final int WS_IO = 0x01; // Sending and reciving websocket data
+    public static final int JSON = 0x02; // Handling and parsing of JSON objects
+    public static final int GENERAL = 0x04; 
+    public static final int WS_PARSE = 0x08; // Parsing of websocket byte arrays
+    public static final int ALL = 0xFF; // Print all. Use as loglevel, not as category
 
     public static int logLevel = 0;
 
@@ -23,4 +23,3 @@ public class Logger {
         }
     }
 }
-
