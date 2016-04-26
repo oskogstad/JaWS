93e202f4cfc55d7afd3b577c67305410d710eeb0
elodin <jorgen.skogstad@gmail.com>
2016-04-26 09:42:05 +0200

    now works with chat.tilfeldig.info

--- src/main/java/jaws/Main.java
+++ src/main/java/jaws/Main.java
@@ -1,17 +1,23 @@
 package jaws;
 import java.io.*;
-import com.google.gson.*;
+import com.google.gson.JsonParser;
+import com.google.gson.JsonElement;
+import com.google.gson.JsonObject;
+
+import org.apache.commons.lang3.*;
 
 public class Main implements WebSocketEventHandler {
 
     private JaWS jaws;
     private JsonParser jsonParser;
+    private StringEscapeUtils stringEscape;
 
     public Main() {
         jaws = new JaWS(40506);
         jaws.setEventHandler(this);
         jaws.start();
         jsonParser = new JsonParser();
+        stringEscape = new StringEscapeUtils();
 
         // ShutdownHook, catches any interrupt signal and closes all threads
         Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
@@ -23,11 +29,15 @@ public class Main implements WebSocketEventHandler {
 
     @Override
     public void onMessage(Connection con, String message) {
-        System.out.println(message);
-        JsonElement json = jsonParser.parse(message);
-        System.out.println(json.toString());
-        
-        jaws.broadcast(message);
+        JsonObject json = (JsonObject) jsonParser.parse(message);
+
+        // escape all text from client
+        json.addProperty("name", StringEscapeUtils.escapeHtml4(json.get("name").getAsString()));
+        json.addProperty("msg", StringEscapeUtils.escapeHtml4(json.get("msg").getAsString()));
+        json.addProperty("timestamp", StringEscapeUtils.escapeHtml4(json.get("timestamp").getAsString()));
+
+        // send to all clients
+        jaws.broadcast(json.toString());
     }
 
     @Override
