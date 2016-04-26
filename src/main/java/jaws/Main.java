2b19d99a8354180c3a63209edd7286b1f1eabfd0
elodin <jorgen.skogstad@gmail.com>
2016-04-26 08:53:10 +0200

    gson now imports, parsing string to JSON seems fine

--- src/main/java/jaws/Main.java
+++ src/main/java/jaws/Main.java
@@ -5,13 +5,14 @@ import com.google.gson.*;
 public class Main implements WebSocketEventHandler {
 
     private JaWS jaws;
-    //private JsonParser jsonParser;
+    private JsonParser jsonParser;
 
     public Main() {
         jaws = new JaWS(40506);
         jaws.setEventHandler(this);
         jaws.start();
-        //jsonParser = new JsonParser();
+        jsonParser = new JsonParser();
+
         // ShutdownHook, catches any interrupt signal and closes all threads
         Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
             public void run() {
@@ -23,8 +24,9 @@ public class Main implements WebSocketEventHandler {
     @Override
     public void onMessage(Connection con, String message) {
         System.out.println(message);
-        //JsonElement json = jsonParser.parse(message);
-        //System.out.println(json.toString());
+        JsonElement json = jsonParser.parse(message);
+        System.out.println(json.toString());
+        
         jaws.broadcast(message);
     }
 
