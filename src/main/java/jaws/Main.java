0f877b2e72722679476c3e991090d40cfa9d17dc
elodin <jorgen.skogstad@gmail.com>
2016-04-26 13:00:32 +0200

    started work on chatlog

--- src/main/java/jaws/Main.java
+++ src/main/java/jaws/Main.java
@@ -1,17 +1,19 @@
 package jaws;
 import java.io.*;
+import java.util.ArrayList;
 import com.google.gson.JsonParser;
 import com.google.gson.JsonElement;
 import com.google.gson.JsonObject;
-
 import org.apache.commons.lang3.*;
 
 public class Main implements WebSocketEventHandler {
-
     private JaWS jaws;
     private JsonParser jsonParser;
     private StringEscapeUtils stringEscape;
     private int numberOfConnections;
+    private ArrayList<String> chatlogArray;
+    private File chatlog;
+    private BufferedReader reader;
 
     public Main() {
         jaws = new JaWS(40506);
@@ -21,6 +23,19 @@ public class Main implements WebSocketEventHandler {
         stringEscape = new StringEscapeUtils();
         numberOfConnections = 0;
 
+        // Read chatlog.txt to chatlogArray
+        chatlogArray = new ArrayList();
+        try {
+            chatlog = new File("chatlog.txt");
+            reader = new BufferedReader(new FileReader(chatlog));
+            String text = null;
+            while ((text = reader.readLine()) != null) {
+                chatlogArray.add(text);
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+        
         // ShutdownHook, catches any interrupt signal and closes all threads
         Runtime.getRuntime().addShutdownHook(new Thread(new Runnable() {
             public void run() {
@@ -44,22 +59,39 @@ public class Main implements WebSocketEventHandler {
 
     @Override
     public void onConnect(Connection con) {
+        for (String s: chatlogArray) {
+            Logger.log(s, Logger.GENERAL);
+        }
+        Logger.log("New connection", Logger.GENERAL);
         numberOfConnections++;
-        // make json and broadcast change to chat.tilfeldig.info
-        System.out.println("number of con: " + numberOfConnections);
+
+        // Broadcast JSON with new numberOfConnections
+        JsonObject json = new JsonObject();
+        json.addProperty("numberOfCon", numberOfConnections);
+        jaws.broadcast(json.toString());
+
+        Logger.log("Number of connections: " + numberOfConnections, Logger.GENERAL);
     }
 
     @Override
     public void onDisconnect(Connection con) {
         Logger.log("Connection disconnected", Logger.GENERAL);
         numberOfConnections--;
-        // make json and broadcast change to chat.tilfeldig.info
-        Logger.log("number of con: " + numberOfConnections, Logger.GENERAL);
+
+        // Broadcast JSON with new numberOfConnections
+        JsonObject json = new JsonObject();
+        json.addProperty("numberOfCon", numberOfConnections);
+        jaws.broadcast(json.toString());
+
+        Logger.log("Number of connections: " + numberOfConnections, Logger.GENERAL);
+    }
+
+    public void writeToChatlog(String text) {
+        // make new thread, write to array and file
     }
 
     public static void main(String[] args) {
         Logger.logLevel = Logger.ALL;
-
         new Main();
     }
 }
